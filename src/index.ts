b280e7a2f5d25f56111bf497216503f05a94cb58
bp26 <aleshak260006@gmail.com>
2023-07-28 08:33:17 +0000

    feat: update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,45 @@
 import * as http from 'http';
+import cluster from 'node:cluster';
+import os from 'os';
 import Router from './routers/Router';
 import 'dotenv/config';
+import { ApiMode } from './utils/types/enums';
 
-http
-  .createServer((req, res) => {
-    if (req.method && req.url) {
-      Router.match(req, res);
+const mode = process.argv.slice(2).includes('-multi') ? ApiMode.MULTI_INSTANCE : ApiMode.SINGLE_INSTANCE;
+process.env.mode = mode;
+
+const createServer = () => {
+  http
+    .createServer((req, res) => {
+      if (req.method && req.url) {
+        Router.match(req, res);
+      }
+    })
+    .listen(process.env.PORT);
+};
+
+switch (mode) {
+  case ApiMode.SINGLE_INSTANCE:
+    createServer();
+    break;
+
+  case ApiMode.MULTI_INSTANCE:
+    if (cluster.isPrimary) {
+      cluster.schedulingPolicy = cluster.SCHED_RR;
+      const cpuCount = os.cpus().length;
+
+      for (let i = 0; i < cpuCount; i++) {
+        cluster.fork();
+      }
+
+      cluster.on('exit', () => {
+        cluster.fork();
+      });
     }
-  })
-  .listen(process.env.PORT);
+
+    if (cluster.isWorker) {
+      createServer();
+    }
+
+    break;
+}
