218f4a06c19933dbedb37566e299a4ac1d8ddd61
bp26 <94074742+bp26@users.noreply.github.com>
2023-07-29 17:56:10 +0300

    fix: fix validateUserData func

--- src/utils/helpers/validateUserData.ts
+++ src/utils/helpers/validateUserData.ts
@@ -1,10 +1,6 @@
 import { UserData } from '../types/interfaces';
 
 export const validateUserData = ({ username, age, hobbies }: UserData) => {
-  if (typeof username !== 'string' || typeof age !== 'number' || !Array.isArray(hobbies) || hobbies.find((item) => typeof item !== 'string')) {
-    return false;
-  }
-
   if (username && typeof username !== 'string') {
     return false;
   }
